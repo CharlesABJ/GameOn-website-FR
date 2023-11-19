0867bc62418d1f113f472a2734f73a92db4169ad
Charles ABJ <charlesabj@Charless-MacBook-Pro.local>
2023-11-19 21:07:05 +0100

    Nettoyage du code html pour la validation html, correction du style pour un meilleur affichage en mobile, ajout d'une fonction pour fermer la modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,37 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelector(".close-modal");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+    main.className += " responsive";
+  } else {
+    x.className = "topnav";
+    main.className -= " responsive";
+  }
+}
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.classList.add("open");
+  if (modalbg.classList.contains("open")) {
+    body.classList.add("modal-open");
+  }
 }
 
+// close modal form
+closeModalBtn.addEventListener("click", closeModal);
 
+function closeModal() {
+  modalbg.classList.remove("open");
+  body.classList.remove("modal-open");
+}
